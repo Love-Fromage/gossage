ddeee80d97a5e1d82bf08e8155492ebd55a8d9d7
mathieu <mathecool22@gmail.com>
2022-03-11 11:31:45 -0500

    working in vs code

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Un commentaire
     console.log("Ça fonctionne!!!");
 
+    const uneFonction = ()=>{
+        console.log('coucou');
+    }
+
+    uneFonction();
+    console.log('test');
+    console.log('test');
+    console.log('test');
+    
 
 });
